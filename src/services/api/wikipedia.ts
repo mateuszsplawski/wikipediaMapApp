e25e2a0eb46ff44e524c9273859b146446794553
Mateusz <mateusz.splawski96@gmail.com>
2021-03-21 15:20:40 +0100

    refactor(wikipedia): Set types for wikipedia api client

--- src/services/api/wikipedia.ts
+++ src/services/api/wikipedia.ts
@@ -1,35 +1,52 @@
 import ky from "ky";
 
+interface wikiQueryParameters {
+  coord: { lat: number; lng: number };
+  radius?: number;
+  limit?: number;
+}
+
+interface wikiGetResponse {
+  batchcomplete: "";
+  query: {
+    geosearch: {
+      dist: number;
+      lat: number;
+      lon: number;
+      ns: number;
+      pageid: number;
+      primary: string;
+      title: string;
+    }[];
+  };
+}
+
 const client = ky.create({
   prefixUrl: "https://en.wikipedia.org/w/",
 });
 
 const wikiApiClient = {
   getArticles({
-    coord = { lat: 52.4082542, lng: 16.9314335 },
+    coord,
     radius = 10000,
     limit = 10,
-  } = {}) {
+  }: wikiQueryParameters): Promise<wikiGetResponse> {
     const params = {
       action: "query",
       list: "geosearch",
       format: "json",
       origin: "*",
     };
-    if (!coord) {
-      console.error("Wikipedia API: no coord passed to getArticles");
-    } else {
-      return client
-        .get(`api.php?`, {
-          searchParams: {
-            ...params,
-            gscoord: coord.lat + "|" + coord.lng,
-            gsradius: radius,
-            gslimit: limit,
-          },
-        })
-        .json();
-    }
+    return client
+      .get(`api.php?`, {
+        searchParams: {
+          ...params,
+          gscoord: coord.lat + "|" + coord.lng,
+          gsradius: radius,
+          gslimit: limit,
+        },
+      })
+      .json();
   },
 };
 
